cb7f03fcab2c9f391a0ff519ef7f902d2b3c2516
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-04-07 23:15:17 +0530

    change image

--- client/src/data/dummyProperties.js
+++ client/src/data/dummyProperties.js
@@ -89,7 +89,7 @@ export const dummyProperties = [
       country: "USA",
     },
     images: [
-      "https://images.unsplash.com/photo-1533323905636-7f0bfa0ba5ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2l0eSUyMHZpZXd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
+      "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     ],
     capacity: {
       guests: 4,
