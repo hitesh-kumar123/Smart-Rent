950a6737b095e8e2b24044a30e862dddb332367f
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-04-07 23:16:03 +0530

    change image

--- client/src/pages/HostListings.jsx
+++ client/src/pages/HostListings.jsx
@@ -51,7 +51,7 @@ const HostListings = () => {
       },
       images: [
         {
-          url: "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+          url: "https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           publicId: "mock1",
         },
       ],
