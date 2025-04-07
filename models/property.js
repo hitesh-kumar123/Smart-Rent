addd89a695bb1908338bd168dd4e3c6aabcb10ad
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-04-07 23:11:22 +0530

    image change

--- models/property.js
+++ models/property.js
@@ -1,61 +1,70 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const propertySchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: [true, 'Title is required'],
-      trim: true
+      required: [true, "Title is required"],
+      trim: true,
     },
     description: {
       type: String,
-      required: [true, 'Description is required']
+      required: [true, "Description is required"],
     },
     category: {
       type: String,
-      required: [true, 'Category is required'],
-      enum: ['Apartment', 'House', 'Villa', 'Condo', 'Cabin', 'Cottage', 'Farmhouse', 'Other']
+      required: [true, "Category is required"],
+      enum: [
+        "Apartment",
+        "House",
+        "Villa",
+        "Condo",
+        "Cabin",
+        "Cottage",
+        "Farmhouse",
+        "Other",
+      ],
     },
     price: {
       type: Number,
-      required: [true, 'Price is required']
+      required: [true, "Price is required"],
     },
     location: {
       type: {
         type: String,
-        enum: ['Point'],
-        default: 'Point'
+        enum: ["Point"],
+        default: "Point",
       },
       coordinates: {
         type: [Number],
-        required: true
+        required: true,
       },
       address: {
         type: String,
-        required: [true, 'Address is required']
+        required: [true, "Address is required"],
       },
       city: {
         type: String,
-        required: [true, 'City is required']
+        required: [true, "City is required"],
       },
       state: {
         type: String,
-        required: [true, 'State is required']
+        required: [true, "State is required"],
       },
       country: {
         type: String,
-        required: [true, 'Country is required']
+        required: [true, "Country is required"],
       },
       zipCode: {
         type: String,
-        required: [true, 'Zip code is required']
-      }
+        required: [true, "Zip code is required"],
+      },
     },
     images: [
       {
         url: String,
-        publicId: String
-      }
+        publicId: String,
+      },
     ],
     amenities: {
       wifi: { type: Boolean, default: false },
@@ -71,88 +80,88 @@ const propertySchema = new mongoose.Schema(
       hotTub: { type: Boolean, default: false },
       breakfast: { type: Boolean, default: false },
       workspace: { type: Boolean, default: false },
-      petFriendly: { type: Boolean, default: false }
+      petFriendly: { type: Boolean, default: false },
     },
     capacity: {
       guests: {
         type: Number,
-        required: [true, 'Number of guests is required'],
-        min: 1
+        required: [true, "Number of guests is required"],
+        min: 1,
       },
       bedrooms: {
         type: Number,
-        required: [true, 'Number of bedrooms is required'],
-        min: 1
+        required: [true, "Number of bedrooms is required"],
+        min: 1,
       },
       beds: {
         type: Number,
-        required: [true, 'Number of beds is required'],
-        min: 1
+        required: [true, "Number of beds is required"],
+        min: 1,
       },
       bathrooms: {
         type: Number,
-        required: [true, 'Number of bathrooms is required'],
-        min: 1
-      }
+        required: [true, "Number of bathrooms is required"],
+        min: 1,
+      },
     },
     rules: {
       smoking: { type: Boolean, default: false },
       pets: { type: Boolean, default: false },
       parties: { type: Boolean, default: false },
-      checkInTime: { type: String, default: '14:00' },
-      checkOutTime: { type: String, default: '11:00' }
+      checkInTime: { type: String, default: "14:00" },
+      checkOutTime: { type: String, default: "11:00" },
     },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+      ref: "User",
+      required: true,
     },
     reviews: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Review'
-      }
+        ref: "Review",
+      },
     ],
     availability: [
       {
         date: Date,
-        isBooked: { type: Boolean, default: false }
-      }
+        isBooked: { type: Boolean, default: false },
+      },
     ],
     averageRating: {
       type: Number,
       default: 0,
       min: 0,
-      max: 5
+      max: 5,
     },
     numReviews: {
       type: Number,
-      default: 0
+      default: 0,
     },
     isActive: {
       type: Boolean,
-      default: true
+      default: true,
     },
     isApproved: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   {
     timestamps: true,
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
 
 // Add index for location-based searches
-propertySchema.index({ 'location.coordinates': '2dsphere' });
+propertySchema.index({ "location.coordinates": "2dsphere" });
 
 // Virtual for calculating availability
-propertySchema.virtual('isAvailable').get(function() {
+propertySchema.virtual("isAvailable").get(function () {
   return this.isActive && this.isApproved;
 });
 
-const Property = mongoose.model('Property', propertySchema);
+const Property = mongoose.model("Property", propertySchema);
 
-module.exports = Property; 
+module.exports = Property;
